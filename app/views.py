eab487dba9539ca9041dd0152577c5b644a2c2ce
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-07 17:27:23 +0300

    Wrote the dashboard view function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, redirect
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, login_required
 
 from app import app
-from app.models import User
+from app.models import User, SensorReading, db
 from app.forms import LoginForm
 
 
@@ -21,3 +21,15 @@ def login():
             return redirect(url_for(endpoint='dashboard'))
 
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    readings_value = db.session.query(SensorReading).order_by(SensorReading.recording_id.desc()).first()
+    sensor_readings = (('Температура', readings_value.temperature),
+                       ('Влажность', readings_value.humidity),
+                       ('Угарный газ', readings_value.carbon_monoxide),
+                       ('Атмосферное давление', readings_value.pressure))
+
+    return render_template('dashboard.html', sensor_readings=sensor_readings, title='dashboard')
