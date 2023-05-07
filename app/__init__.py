e9ff972b451b24bf11b1c4dfa657020f80fbf412
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-07 12:38:26 +0300

    Connected the Flasj login module

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 db = SQLAlchemy(app)
 
 from . import views
