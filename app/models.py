84c950083ddd0560c696d44583ff259aaeb5e2cc
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-07 12:34:45 +0300

    Changed models for authorization

--- app/models.py
+++ app/models.py
@@ -1,23 +1,35 @@
-from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,  check_password_hash
+
+from . import db, login_manager
+
 from datetime import datetime
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(500), nullable=False)
+    password_hash = db.Column(db.String(500), nullable=False)
     user_name = db.Column(db.String(30), nullable=False)
     user_last_name = db.Column(db.String(30), nullable=False)
-    active = db.Column(db.Boolean, nullable=False)
     data_register = db.Column(db.DateTime, default=datetime.utcnow)
     role = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
     telegram_login = db.Column(db.String(30), unique=True)
     telegram_notifications = db.Column(db.Boolean)
 
+    def get_id(self):
+        return self.user_id
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
-        return f'User(id={self.id}, email={self.email}, user_name={self.user_name}'
+        return f'User(id={self.user_id}, email={self.email}, user_name={self.user_name}'
 
 
 class Role(db.Model):
@@ -41,3 +53,8 @@ class ServerAccessToken(db.Model):
 
     def __repr__(self):
         return f"ServerAccessToken(token_id={self.token_id}, token_name={self.token_name})"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
