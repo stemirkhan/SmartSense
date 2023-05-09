3c10243ac1eddbb2ee2a4cb56183eb769fbca971
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-09 17:53:22 +0300

    Added an environment variable to access the database

--- config.py
+++ config.py
@@ -5,15 +5,20 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class DevelopementConfig(BaseConfig):
+class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI')
+
 
 
 class TestingConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TESTING_DATABASE_URI')
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI')
