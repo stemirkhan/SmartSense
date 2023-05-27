665bfc2012246b57b1c3731def5d2f3d08538025
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-05-27 17:41:38 +0300

    added server error page

--- app/errors/routes.py
+++ app/errors/routes.py
@@ -6,3 +6,9 @@ from . import bp_errors
 @bp_errors.app_errorhandler(404)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
+
+
+@bp_errors.app_errorhandler(500)
+def page_not_found(e):
+    return render_template('errors/500.html'), 500
+
