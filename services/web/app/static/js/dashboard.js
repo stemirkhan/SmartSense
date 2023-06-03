e5b45130de7ddf2005a23fd5dc8faf5df15c48ee
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-06-03 23:21:22 +0300

    Added a loop through the ascending data

--- services/web/app/static/js/dashboard.js
+++ services/web/app/static/js/dashboard.js
@@ -1,17 +1,18 @@
 function fetchdata(){
- $.ajax({
-  url: '/dashboard',
-  type: 'get',
-  contentType: 'application/json',
-  success: function(response){
-   document.getElementById("temperature").innerHTML = response.temperature;
-   document.getElementById("humidity").innerHTML = response.humidity;
-   document.getElementById("carbon_monoxide").innerHTML = response.carbon_monoxide;
-   document.getElementById("atmosphere_pressure").innerHTML = response.atmosphere_pressure;
-  }
- });
-}
-
-$(document).ready(function(){
- setInterval(fetchdata,2000);
-});
+    $.ajax({
+     url: '/dashboard',
+     type: 'get',
+     contentType: 'application/json',
+     success: function(response){
+     for (var key in response) {
+       if (response.hasOwnProperty(key)) {
+           document.getElementById(key).innerHTML = response[key];
+       }
+       }
+     }
+    });
+   }
+   
+   $(document).ready(function(){
+    setInterval(fetchdata,2000);
+   });
